c77cf262184083a0cac61350bbb26515a7992949
Nikita Churkin <n.churkin@matriks.group>
2025-08-26 23:39:07 +0300

    add vue router, vite and tailwind

---
+++ resources/js/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h1 class="text-3xl font-bold underline">
+        Hello world
+    </h1>
+    <router-link to="index">Index</router-link>
+    <router-link to="page">Page</router-link>
+    <router-view></router-view>
+</template>
+<script>
+    export default {
+        name: "App"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/views/Page.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        page
+    </div>
+</template>
+
+<style scoped>
+
+</style>
